335fc1b7646524c2520ace04f26cc42c35662d0e
10121999 <esme_lokis_123@live.com.mx>
2016-04-14 09:25:46 -0500

    version 1.0
    vibrar

---
+++ acciones.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$(document).ready(function (e){
+	document.addEventlistener("deviceready",function(){
+		$(´#beep´).tap(function(){
+				navigator.notification.beep(1);
+		});//Tap del beep
+		$(´#vibrar´).tap(function()}
+				navigator.notication.vibrate(1000);
+				});//Tap del beep
+				},false);//deviceready
+				});//ready
+				VIBRABEEP
+				
+				
